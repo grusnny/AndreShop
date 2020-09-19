45a7d15f43250574fa218e7959167a6aa1f55566
grusnny <37426267+grusnny@users.noreply.github.com>
2020-09-18 20:23:06 -0500

    producto

--- src/Producto/InfoProducto.js
+++ src/Producto/InfoProducto.js
@@ -3,26 +3,13 @@ import React, { Component } from 'react'
 
 class InfoProducto extends Component {
     state = {  
-        info:this.props.infoProducto
+        info:this.props.infoProducto(),
+        desc:this.props.descripProducto()
     }
     render() {
-        const[title,price,original_price,available_quantity,pictures,]=this.state.info
+
         return (
-            <div className="container text-center">
- 
-                    <div class="container text-center shadow">
-                        <div className="overflow">
-                        {/* <img src={} alt=""  className="card-img-top"/> */}
-                        </div>
-                        <div className="text-dark">
-                            <h4 className="card-title">{'$'+this.state.info.price}
-                            {/* <div className="ui-item__discount-text "> {parseInt(((precio/original_price)*100))+'% OFF'}</div> */}
-                            </h4>
-                            <h6 className="card-title">{title} </h6>
-                            <p  className="card-text text-secondary">
-                            </p>
-                        </div>
-                    </div>
+            <div className=" container-fluid">
             </div>
         );
     }
--- src/Producto/Productos.js
+++ src/Producto/Productos.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ProductoDescrip from './InfoProducto';
+import './card-style.css';
 
 class Productos extends Component {
     state = {  
@@ -13,26 +14,40 @@ class Productos extends Component {
 
         fetch (url)
             .then(respuesta =>respuesta.json())
-            .then(resultado =>this.setState({infoProducto:resultado.results}))
-
-        return this.state.infoProducto
+            .then(resultado =>this.setState({informacionProducto:resultado}))
+        
     }
-    descripcionProducto=()=>{
+    descripProducto=()=>{
         const id=this.state.producto;
         const url= `https://api.mercadolibre.com/items/${id}/description`;
 
         fetch (url)
             .then(respuesta =>respuesta.json())
-            .then(resultado =>this.setState({descripcionProducto:resultado.results}))
+            .then(resultado =>this.setState({descripcionProducto:resultado}))
         
-        return this.state.descripcionProducto
+        
     }
     render() {
         return (
-            <div className="container text-center">
-                {this.infoProducto()}
-                {this.state.informacionProducto.title}
-                <ProductoDescrip infoProducto={this.infoProducto}/>
+            <div className="container-fluid">
+                <ProductoDescrip descripProducto={this.descripProducto} infoProducto={this.infoProducto}/>
+                <div className="container" >
+                
+                    <div class="card-body shadow" >
+                        <div className="overflow">
+                         <img src={this.state.informacionProducto.thumbnail} alt=""  className="card-img-top"/> 
+                        </div>
+                        <div className=" text-center text-dark">
+                            <h4 className="card-title">{this.state.informacionProducto.title}
+                            {/* <div className="ui-item__discount-text "> {parseInt(((precio/original_price)*100))+'% OFF'}</div> */}
+                            </h4>
+                            <h5 className="card-title">${this.state.informacionProducto.price} </h5>
+                            <p  className="card-text text-secondary">
+                            {this.state.descripcionProducto.plain_text}
+                            </p>
+                        </div>
+                    </div>
+            </div>
             </div>
         );
     }
