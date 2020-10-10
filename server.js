5e02544a1053cdea958de0cf3d40ce583878c142
grusnny <37426267+grusnny@users.noreply.github.com>
2020-10-10 12:38:39 -0500

    cambios

--- server.js
+++ server.js
@@ -14,3 +14,5 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
+
+//  "start": "react-scripts start",  cambiar esto en packeje.json para compilar 
