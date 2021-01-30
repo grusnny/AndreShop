fec2a4e0779ee15daf56f1a424028d702cc8f055
grusnny <37426267+grusnny@users.noreply.github.com>
2021-01-30 11:54:12 -0500

    Update sample_spec.js

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,11 +3,12 @@ describe('AndresShop', ()=>{
 
     it('first cypress test on a webpage,()',()=>{
         cy.visit('http://localhost:8080/busqueda'); //ARRAGE
-        cy.get('class="ui input"').type('iPhone {enter}')
+        cy.get('.jumbotron').get('form')
+        cy.get('.ui input').type('iPhone {enter}')
         cy.wait(3000)
 
-        cy.contains('iPhone').click();//ACT
-        cy.url().should('include','commands/action');//ASSERT
+        cy.contains('iPhone').get('.btn-outline-success:first').click();
+        cy.url().should('include','/producto');//ASSERT
         
     })
 })
